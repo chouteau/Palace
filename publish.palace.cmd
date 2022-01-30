a2adf98787167197c92870fb104aca8d77b91d5e
Marc Chouteau <marc_chouteau@hotmail.com>
2022-01-30 17:25:04 +0100

    yaml

--- publish.palace.cmd
+++ publish.palace.cmd
@@ -1,6 +1,6 @@
+rd src\dotnet6\palace\bin\debug\net6.0\publish /s /Q
 dotnet publish src\dotnet6\palace\palace.csproj
 e:
 cd src\dotnet6\palace\bin\debug\net6.0\publish
-"C:\Program Files\7-Zip\7z.exe" a -xr!*.json -tzip -r ..\..\..\..\..\..\..\publish\palace.zip *
+"C:\Program Files\7-Zip\7z.exe" a -x!*.json -tzip -r ..\..\..\..\..\..\..\publish\palace.zip *
 explorer "..\..\..\..\..\..\..\publish\"
-exit
