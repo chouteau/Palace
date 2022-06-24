79417f81a3d783c31af0758500dac089da0668b6
Marc Chouteau <marc_chouteau@hotmail.com>
2022-06-24 15:48:40 +0200

    Cleanup main branch , remove dotnet4.7 version, moved to unmaintained branch

--- deploy.cmd
+++ deploy.cmd
@@ -1,6 +1,6 @@
 dotnet publish src\dotnet6\demosvc\demosvc.csproj
 e:
-cd src\dotnet6\DemoSvc\bin\Debug\net6.0\publish
-"C:\Program Files\7-Zip\7z.exe" a -tzip -r ..\..\..\..\..\..\..\palaceserver\temp\demosvc.zip *
-xcopy /Y ..\..\..\..\..\..\..\palaceserver\temp\demosvc.zip  ..\..\..\..\..\..\..\palaceserver\staging\demosvc.zip
+cd src\DemoSvc\bin\Debug\net6.0\publish
+"C:\Program Files\7-Zip\7z.exe" a -tzip -r ..\..\..\..\..\..\palaceserver\temp\demosvc.zip *
+xcopy /Y ..\..\..\..\..\..\palaceserver\temp\demosvc.zip  ..\..\..\..\..\..\palaceserver\staging\demosvc.zip
 exit
